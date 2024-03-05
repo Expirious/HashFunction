3cef8e0121d5b495d7442c26f8707c626af6408f
Expirious <66459071+Expirious@users.noreply.github.com>
2024-03-05 17:34:55 +0200

    Add files via upload

---
+++ Hashing Algorithm/Hashing Algorithm/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Hashing_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalText = new System.Windows.Forms.RichTextBox();
+            this.hashButton = new System.Windows.Forms.Button();
+            this.HMACtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hashText = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // originalText
+            // 
+            this.originalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalText.Location = new System.Drawing.Point(12, 12);
+            this.originalText.Name = "originalText";
+            this.originalText.Size = new System.Drawing.Size(970, 262);
+            this.originalText.TabIndex = 0;
+            this.originalText.Text = "";
+            // 
+            // hashButton
+            // 
+            this.hashButton.Location = new System.Drawing.Point(426, 305);
+            this.hashButton.Name = "hashButton";
+            this.hashButton.Size = new System.Drawing.Size(89, 41);
+            this.hashButton.TabIndex = 1;
+            this.hashButton.Text = "Hash";
+            this.hashButton.UseVisualStyleBackColor = true;
+            this.hashButton.Click += new System.EventHandler(this.hashButton_Click);
+            // 
+            // HMACtextBox
+            // 
+            this.HMACtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HMACtextBox.Location = new System.Drawing.Point(79, 311);
+            this.HMACtextBox.Name = "HMACtextBox";
+            this.HMACtextBox.Size = new System.Drawing.Size(218, 26);
+            this.HMACtextBox.TabIndex = 2;
+            this.HMACtextBox.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HMAC:";
+            this.label1.Visible = false;
+            // 
+            // hashText
+            // 
+            this.hashText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hashText.Location = new System.Drawing.Point(12, 372);
+            this.hashText.Name = "hashText";
+            this.hashText.Size = new System.Drawing.Size(970, 262);
+            this.hashText.TabIndex = 4;
+            this.hashText.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(17, 281);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Enable HMAC";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 646);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.hashText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HMACtextBox);
+            this.Controls.Add(this.hashButton);
+            this.Controls.Add(this.originalText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox originalText;
+        private System.Windows.Forms.Button hashButton;
+        private System.Windows.Forms.TextBox HMACtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox hashText;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Hashing Algorithm/Hashing Algorithm/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hashing_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int EulerFunction(string text)
+        {
+            int n = text.Length;
+            int result = n;
+
+            for (int p = 2; p * p <= n; ++p)
+            {
+
+                if (n % p == 0)
+                {
+
+                    while (n % p == 0)
+                        n /= p;
+                    result -= result / p;
+                }
+            }
+
+            if (n > 1)
+                result -= result / n;
+            return result;
+        }
+
+        private string Encrypt(string text, int n)
+        {
+            string newText = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                char c = text[i % n];
+
+                if (c == ' ')
+                {
+                    int j = i;
+                    while (c == ' ')
+                    {
+                        j++;
+                        c = text[j % n];
+                    }
+                }
+                
+                newText += c;
+            }
+
+            return text;
+        }
+
+        private int Sum(string text)
+        {
+            int result = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                result += text[i];
+            }
+
+            return result;
+        }
+
+        private string Hash(string text, int n, string HMAC)
+        {
+            string hashSymbols = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfhgjlkzcxvnbnm1234567890" + HMAC;
+            string hash = "";
+
+            for (int i = 0; i < 40; i++)
+            {
+                int index = (text.Length + n * i) % hashSymbols.Length;
+
+                while (hashSymbols[index] == ' ')
+                {
+                    index = (index + 1) % hashSymbols.Length;
+                }
+
+                hash += hashSymbols[index];
+            }
+
+            return hash;
+        }
+
+        private void hashButton_Click(object sender, EventArgs e)
+        {
+            string HMAC = "";
+            if (checkBox1.Checked)
+                HMAC = HMACtextBox.Text;
+
+            string hash = originalText.Text;
+            int n = EulerFunction(hash);
+
+            hash = Encrypt(hash, n);
+            n = Sum(hash);
+            hash = Hash(hash, n, HMAC);
+
+            hashText.Text = hash;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label1.Visible = true;
+                HMACtextBox.Visible = true;
+            }
+            else
+            {
+                label1.Visible = false;
+                HMACtextBox.Visible = false;
+            }
+        }
+    }
+}
